421c4c024cd21edafb202f35e29e8b82bbf1a926
dilowagner <diegowagner4@gmail.com>
2017-04-15 23:24:22 -0300

    Enviando atualizacao do Service

--- src/Call/Service.php
+++ src/Call/Service.php
@@ -12,6 +12,28 @@ class Service
         $this->client = $client;
     }
 
+    /**
+     * @param string $originNumber
+     * @param string $destinyNumber
+     * @param bool $isSaveAudio
+     * @param string $originBina
+     * @param string $destinyBina
+     * @param string $tags
+     * @return mixed
+     */
+    public function call($originNumber, $destinyNumber, $isSaveAudio = false, $originBina = null, $destinyBina = null, $tags = null)
+    {
+        $data = [
+            'numero_origem'  => $originNumber,
+            'numero_destino' => $destinyNumber,
+            'gravar_audio'   => $isSaveAudio,
+            'bina_origem'    => $originBina,
+            'bina_destino'   => $destinyBina,
+            'tags'           => $tags
+        ];
+        return $this->client->post('/chamada/', $data);
+    }
+
     public function getCallById($id)
     {
         return $this->client->get('/chamada/' . $id);
