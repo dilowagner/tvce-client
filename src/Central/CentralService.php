fa386538d6ae0d1de8fdd13f5e26de53a1b86cd0
dilowagner <diegowagner4@gmail.com>
2017-04-16 18:20:21 -0300

    Enviando CentralService

---
+++ src/Central/CentralService.php
@@ -0,0 +1,122 @@
+<?php
+namespace Tvce\Central;
+
+use Tvce\Path;
+use Tvce\SocketClientInterface;
+
+class CentralService
+{
+    /**
+     * @var string
+     */
+    const RAMAL_ROUTE = '/ramal/';
+
+    /**
+     * @var string
+     */
+    const WEBPHONE_ROUTE = '/webphone/';
+
+    /**
+     * @var SocketClientInterface
+     */
+    private $client;
+
+    /**
+     * Service constructor.
+     * @param SocketClientInterface $client
+     */
+    public function __construct(SocketClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return string
+     */
+    public function create($ramal = null, $login = null, $password = null, $bina = null, $isExternal = false, $isCelular = false, $isSaveAudio = false, $isAccessRecording = false)
+    {
+        $data = [
+            'ramal'  => $ramal,
+            'login' => $login,
+            'senha' => $password,
+            'bina' => $bina,
+            'ligacao_externa' => $isExternal,
+            'ligacao_celular' => $isCelular,
+            'gravar_audio' => $isSaveAudio,
+            'acesso_gravacoes' => $isAccessRecording
+        ];
+
+        $path = new Path([self::RAMAL_ROUTE]);
+        return $this->client->post($path->build(), $data);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function remove($id)
+    {
+        $path = new Path([self::RAMAL_ROUTE, $id]);
+        return $this->client->delete($path->build());
+    }
+
+    /**
+     * @return string
+     */
+    public function get($id)
+    {
+        $path = new Path([self::RAMAL_ROUTE, $id]);
+        return $this->client->get($path->build());
+    }
+
+    /**
+     * @return string
+     */
+    public function update($id, $ramal = null, $login = null, $password = null, $bina = null, $isExternal = false, $isCelular = false, $isSaveAudio = false, $isAccessRecording = false)
+    {
+        $data = [
+            'ramal'  => $ramal,
+            'login' => $login,
+            'senha' => $password,
+            'bina' => $bina,
+            'ligacao_externa' => $isExternal,
+            'ligacao_celular' => $isCelular,
+            'gravar_audio' => $isSaveAudio,
+            'acesso_gravacoes' => $isAccessRecording
+        ];
+
+        $path = new Path([self::RAMAL_ROUTE, $id]);
+        return $this->client->put($path->build(), $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function report()
+    {
+        $path = new Path([self::RAMAL_ROUTE, 'relatorio']);
+        return $this->client->get($path->build());
+    }
+
+    /**
+     * @param $type
+     * @param $idRamal
+     * @param $ramal
+     * @param $callFor
+     * @param $isClosedEnd
+     * @return string
+     */
+    public function webphone($type, $idRamal, $ramal, $callFor, $isClosedEnd)
+    {
+        $params = [
+            'tipo' => $type,
+            'id_ramal' => $idRamal,
+            'ramal' => $ramal,
+            'ligar_para' => $callFor,
+            'fechar_fim' => $isClosedEnd
+        ];
+
+        $path = new Path([self::WEBPHONE_ROUTE]);
+        return $this->client->get($path->build(), $params);
+    }
+}
